6a7d95ea7dcf050a16865420c7fab657654b3391
Christian Kalkhoff <kalkhoff@fundsaccess.com>
2022-04-06 10:27:30 +0200

    Add example how to reproduce the bug

---
+++ src/main/java/com/acme/foobar/port/http/api/v20220406/FooClass.java
@@ -0,0 +1,9 @@
+package com.acme.foobar.port.http.api.v20220406;
+
+public class FooClass {
+
+  public void doFoo() {
+
+  }
+
+}
---
+++ src/test/java/com/acme/foobar/port/http/api/v20220406/FooClassTest.java
@@ -0,0 +1,14 @@
+package com.acme.foobar.port.http.api.v20220406;
+
+import com.adelean.inject.resources.junit.jupiter.TestWithResources;
+import org.junit.jupiter.api.Test;
+
+@TestWithResources
+class FooClassTest {
+
+  @Test
+  void testFoo() {
+
+  }
+
+}
